e1f73b103d893802cebea05dc94eb247c318ce17
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-06-26 00:57:09 -0500

    Use examples instead of source code files

---
+++ bbx_sandbox/src/constants.rs
@@ -0,0 +1,2 @@
+pub const SAMPLE_RATE: usize = 44100;
+pub const PLAYTIME_DURATION: u64 = 3;
--- bbx_sandbox/src/graphs/sine_wave.rs
+++
@@ -1,7 +0,0 @@
-use bbx_dsp::{block::Block, generator::Generator, graph::Graph};
-
-pub fn create_sine_wave(graph: &mut Graph) {
-    let mut oscillator = Generator::new(graph.sample_rate());
-    oscillator.set_frequency(220.0);
-    graph.add_block(Block::Generator(oscillator));
-}
