4950b4cd8e74871b6a54fc30840f458a1d5feb17
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-11-22 10:31:38 -0600

    Draw better line depicting phasor lines

--- bbx_dsp/src/phasor.rs
+++ bbx_dsp/src/phasor.rs
@@ -1,29 +1,38 @@
 pub struct Phasor {
-    px: f32,
-    py: f32,
+    ix: f32,
+    iy: f32,
 }
 
 impl Phasor {
     pub fn new() -> Phasor {
         Phasor {
-            px: 0.0,
-            py: 0.0,
+            ix: 0.5,
+            iy: 0.5,
         }
     }
 }
 
 impl Phasor {
     pub fn apply(&self, phase: f32) -> f32 {
-        if phase <= self.px {
-            phase * self.py / self.px
+        if phase <= self.ix {
+            phase * self.iy / self.ix
         } else {
-            let slope = (1.0 - self.py) / (1.0 - self.px);
-            ((phase - self.px) * slope) + self.py
+            let slope = (1.0 - self.iy) / (1.0 - self.ix);
+            ((phase - self.ix) * slope) + self.iy
         }
     }
 
+    // pub fn add_pivot(&mut self, pivot: (f32, f32)) {
+    //     self.inflections.push(pivot);
+    //     self.inflections.sort_by(|p1, p2| p1.0.partial_cmp(&p2.0).unwrap());
+    // }
+
+    pub fn get_inflection(&self) -> (f32, f32) {
+        (self.ix, self.iy)
+    }
+
     pub fn set_pivot(&mut self, x: f32, y: f32) {
-        self.px = x.clamp(0.0, 1.0);
-        self.py = y.clamp(0.0, 1.0);
+        self.ix = x.clamp(0.0, 1.0);
+        self.iy = y.clamp(0.0, 1.0);
     }
 }
