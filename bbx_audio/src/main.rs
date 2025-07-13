50bb35a3d059e7a0d89bae72ab39696680bb804d
Matthew Maxwell <maxwellmattryan@gmail.com>
2025-07-14 01:47:49 +0900

    Get to working state

---
+++ bbx_audio/src/main.rs
@@ -0,0 +1,25 @@
+use bbx_audio::{Graph, GraphBuilder, Player, Signal, Waveform};
+
+// EXAMPLE
+fn create_graph() -> Graph<f32> {
+    let mut builder = GraphBuilder::new(44100.0, 512, 2);
+
+    let oscillator = builder.add_oscillator(440.0, Waveform::Sine);
+
+    let lfo = builder.add_lfo(2.0, 50.0);
+    builder.modulate(lfo, oscillator, "Frequency");
+
+    let output = builder.add_output(2);
+
+    builder.connect(oscillator, 0, output, 0);
+    builder.connect(oscillator, 0, output, 1);
+
+    builder.build()
+}
+
+fn main() {
+    let graph = create_graph();
+    let signal = Signal::new(graph);
+    let player = Player::new(signal);
+    player.play(Some(2));
+}
--- bbx_midi/src/error.rs
+++
@@ -1,7 +0,0 @@
-pub type Result<T> = std::result::Result<T, BbxAudioMidiError>;
-
-#[derive(Debug, PartialEq, thiserror::Error)]
-pub enum BbxAudioMidiError {
-    #[error("missing MIDI input port")]
-    MissingMidiInputPort,
-}
